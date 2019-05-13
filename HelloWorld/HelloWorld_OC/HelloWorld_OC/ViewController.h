a3ac75b9fbe0dac2802c0a25767aa9a13c4dfd3f
Charles <changbai1224@gmail.com>
2019-05-12 20:34:31 -0700

    Objective C initial

---
+++ HelloWorld/HelloWorld_OC/HelloWorld_OC/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  HelloWorld_OC
+//
+//  Created by Charles  on 4/15/19.
+//  Copyright © 2019 Charlie.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+    
+@property (weak, nonatomic) IBOutlet UILabel *label;
+    
+- (IBAction)Cat:(id)sender;
+    
+
+@end
+
